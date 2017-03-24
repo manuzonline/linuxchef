af59ce5d960365b223ff632ad39d3b7325ac95fc
manuzonline <ucngtme@gmail.com>
2017-03-24 17:32:59 +0000

    adding texteditors

---
+++ cookbooks/texteditors/README.md
@@ -0,0 +1,4 @@
+# texteditors
+
+TODO: Enter the cookbook description here.
+
---
+++ cookbooks/texteditors/recipes/default.rb
@@ -0,0 +1,15 @@
+#
+# Cookbook:: texteditors
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved
+package 'vim' do
+action [:install]
+end
+
+
+package 'nano' do
+action [:install]
+end 
+ 
+
